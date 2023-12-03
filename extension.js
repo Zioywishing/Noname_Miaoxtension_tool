107c501755dfc79e1042e7db77c3a573f1b4f890
Miao_Spring <539943419@qq.com>
2023-12-03 12:26:20 +0800

    .

--- extension.js
+++ extension.js
@@ -454,8 +454,8 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 
 			
 			// alert(lib.config.extension_喵喵配件_player_number_33)
-			if(lib.config.extension_喵喵配件_player_number_33){
-				setInterval(()=>{
+			// if(lib.config.extension_喵喵配件_player_number_33){
+			// 	setTimeout(()=>{
 					Object.defineProperty(lib.mode.identity.config.player_number, "item", {
 						get: function () {
 							const minimumNumberOfPlayers=2,maximumNumberOfPlayers=Math.max(_status.maximumNumberOfPlayers||10,minimumNumberOfPlayers,33),item={};
@@ -466,8 +466,8 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 						},
 					});
 					// alert(1)
-				},2000)
-			}
+			// 	},2000)
+			// }
 			
 			
 			miao_nodelay = function(){
@@ -649,7 +649,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 
 			//四字适配
 			if (lib.config.extension_喵喵配件_nodeintroFix) {
-				setInterval(()=>{
+				setTimeout(()=>{
 				get.nodeintro = function (node, simple, evt) {
 					var uiintro = ui.create.dialog("hidden", "notouchscroll");
 					if (node.classList.contains("player") && !node.name) {
@@ -1764,7 +1764,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 						lib.setScroll(uiintro.contentContainer);
 					}
 					return uiintro;
-				}},2000)
+				}},1)
 			}
 		},
 		precontent: function () {},
@@ -1775,18 +1775,18 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
 				intro: "开启此选项后游戏速度可选择无延迟，大大加快ai出牌速度。",
 				init: true
 			},
-			
+
 			nodeintroFix: {
 				name: "get.nodeintro修正",
 				intro: "开启此选项可正确显示超过2个字的技能名称。仅测试了军八环境",
 				init: true
 			},
 
-			player_number_33: {
-				name: "人数上限修改",
-				intro: "开启此选项可将人数上限修改为33人",
-				init: true
-			},
+			// player_number_33: {
+			// 	name: "人数上限修改",
+			// 	intro: "开启此选项可将人数上限修改为33人",
+			// 	init: true
+			// },
 
 			//存在风险
 
